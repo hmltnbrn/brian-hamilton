51e3f5028be3cd80e5201c5b0c907bcc085bf8d3
Brian Hamilton <hmltnbrn@gmail.com>
2018-06-09 21:57:32 -0400

    New footer/banner

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -6,10 +6,10 @@ class Banner extends React.Component {
   render() {
 
     return (
-      <div id="banner" className="Banner-container">
-        <div className="Banner-words">
-          <div className="Banner-name">Brian Hamilton</div>
-          <div className="Banner-title">web developer</div>
+      <div id="banner" className="banner-container">
+        <div className="banner-words">
+          <div className="banner-name">Brian Hamilton</div>
+          <div className="banner-title">web developer</div>
          </div>
       </div>
     );
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import './Footer.css';
 
+import { Link } from 'react-router-dom';
+
+import { TwitterIcon } from '../../icons/Twitter';
+import { LinkedInIcon } from '../../icons/LinkedIn';
+import { GitHubIcon } from '../../icons/GitHub';
+
 class Footer extends React.Component {
 
   render() {
@@ -8,7 +14,19 @@ class Footer extends React.Component {
     return (
       <footer>
         <p className="footer-name">Brian Hamilton © 2016-2018</p>
-        <p className="footer-repo">View this site&rsquo;s <a href="https://github.com/hmltnbrn/brian-hamilton" target="_blank" rel="noopener noreferrer">repository</a> | Photo by <a href="https://www.flickr.com/photos/tedchambers/" target="_blank" rel="noopener noreferrer">Ted Chambers</a></p>
+        <div className="footer-nav">
+          <Link to="/" className="footer-link">Home</Link>
+          <Link to="/resume" className="footer-link">Resume</Link>
+          <Link to="/portfolio" className="footer-link">Portfolio</Link>
+          <Link to="/contact" className="footer-link">Contact</Link>
+        </div>
+        <div className="footer-nav">
+          <a href="tel:1-518-391-5033"><i className="material-icons">phone</i></a>
+          <a href="mailto:hmltnbrn@gmail.com"><i className="material-icons">email</i></a>
+          <a href="https://twitter.com/hmltnbrn" target="_blank" rel="noopener noreferrer"><TwitterIcon /></a>
+          <a href="https://www.linkedin.com/in/brian-hamilton-520835a8" target="_blank" rel="noopener noreferrer"><LinkedInIcon /></a>
+          <a href="https://github.com/hmltnbrn" target="_blank" rel="noopener noreferrer"><GitHubIcon /></a>
+        </div>
       </footer>
     );
   }
