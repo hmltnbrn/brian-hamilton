b4613a14f0b0271f0b48f39f80efc45c44de052b
Brian Hamilton <hmltnbrn@gmail.com>
2018-06-13 19:29:38 -0400

    Continue resume

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -14,21 +14,41 @@ class Resume extends React.Component {
           <div className="section-right">
             <div className="side-by-side">
               <div>
-                <h2>Languages/Frameworks</h2>
+                <h2>Software Development</h2>
                 <ul>
-                  <li>HTML5</li>
-                  <li>CSS3 / SASS</li>
-                  <li>JavaScript / jQuery</li>
-                  <li>PHP</li>
-                  <li>D3.js</li>
-                  <li>Python 2.7+</li>
-                  <li>C++</li>
-                  <li>Node.js</li>
-                  <li>React</li>
-                  <li>Angular 2+</li>
-                  <li>PostgreSQL</li>
-                  <li>MySQL</li>
-                  <li>Bootstrap</li>
+                  <li>Web Design
+                    <ul>
+                      <li>HTML5</li>
+                      <li>CSS3 / SASS</li>
+                      <li>JavaScript / jQuery</li>
+                      <li>PHP</li>
+                      <li>Bootstrap</li>
+                    </ul>
+                  </li>
+                  <li>Web Frameworks
+                    <ul>
+                      <li>Node.js / Express</li>
+                      <li>React</li>
+                      <li>Angular 2+</li>
+                    </ul>
+                  </li>
+                  <li>Database/Data Visualization
+                    <ul>
+                      <li>PostgreSQL</li>
+                      <li>MySQL</li>
+                      <li>MongoDB</li>
+                      <li>Oracle</li>
+                      <li>Tableau</li>
+                      <li>D3.js</li>
+                      <li>JSON / XML</li>
+                    </ul>
+                  </li>
+                  <li>Object-Oriented
+                    <ul>
+                      <li>Python</li>
+                      <li>C++</li>
+                    </ul>
+                  </li>
                 </ul>
               </div>
               <div>
@@ -39,6 +59,7 @@ class Resume extends React.Component {
                   <li>Responsive Web Design</li>
                   <li>Cross-Browser Compatibility</li>
                   <li>Mobile Web Design</li>
+                  <li>Database Normalization</li>
                 </ul>
               </div>
             </div>
@@ -52,10 +73,39 @@ class Resume extends React.Component {
             <div className="resume-experience">
               <h2><span className="primary-color">Software Engineer</span> <span className="company-name">DGDean</span></h2>
               <h3>New York, New York | July 2017 - Present</h3>
+              <ul>
+                <li>Co-developer for Sweet Defeat platform.</li>
+                <li>Developed an internal data manager for Steady, using Node.js and Angular 5.</li>
+                <li>Worked on the back-end API for Steady.</li>
+              </ul>
             </div>
             <div className="resume-experience">
               <h2><span className="primary-color">Consultant Web Developer</span> <span className="company-name">New York City Transit</span></h2>
               <h3>New York, New York | March 2015 - March 2017</h3>
+              <ul>
+                <li>Built data visualizations for subway and bus performance, using BIRT reports, JavaScript, jQuery, HTML5, and SQL.</li>
+                <li>Created a dashboard for real-time data display, using Node.js, React.js, Socket.IO, and RabbitMQ.</li>
+                <li>Worked on adapting open-source projects for visualizing scheduled and real-time train movement, using Python, JavaScript (D3.js library), JSON, SQL, and Bootstrap.</li>
+                <li>Queried from an Oracle database, which included data for subway and bus ridership, daily arrival and departure times, and station information.</li>
+                <li>Used real-time and scheduled data feeds to build static JSON files.</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div className="resume-section">
+          <div className="section-left">
+            <h1>Education</h1>
+          </div>
+          <div className="section-right">
+            <div className="resume-education">
+              <h2><span className="primary-color">Graduate Center at the City University of New York</span></h2>
+              <h3>New York, New York | 2016 - Present</h3>
+              <p>Studying for a Master of Arts in Liberal Studies, specializing in data visualization and digital humanities.</p>
+            </div>
+            <div className="resume-education">
+              <h2><span className="primary-color">Rensselaer Polytechnic Institute</span></h2>
+              <h3>Troy, New York | 2010 - 2014</h3>
+              <p>Graduated as a Bachelor of Science in Information Technology and Web Science, with a concentration in Civil Engineering.</p>
             </div>
           </div>
         </div>
