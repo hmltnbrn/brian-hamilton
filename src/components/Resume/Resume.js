fb61caed5678ca523c1ecf599357b40223fa80b4
Brian Hamilton <hmltnbrn@gmail.com>
2018-06-13 20:34:05 -0400

    Finish resume text

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -14,7 +14,7 @@ class Resume extends React.Component {
           <div className="section-right">
             <div className="side-by-side">
               <div>
-                <h2>Software Development</h2>
+                <h2 className="primary-color">Software Development</h2>
                 <ul>
                   <li>Web Design
                     <ul>
@@ -52,14 +52,14 @@ class Resume extends React.Component {
                 </ul>
               </div>
               <div>
-                <h2>Methodologies</h2>
+                <h2 className="primary-color">Methodologies</h2>
                 <ul>
                   <li>Agile Software Development</li>
-                  <li>REST API</li>
-                  <li>Responsive Web Design</li>
                   <li>Cross-Browser Compatibility</li>
-                  <li>Mobile Web Design</li>
                   <li>Database Normalization</li>
+                  <li>Mobile Web Design</li>
+                  <li>Responsive Web Design</li>
+                  <li>REST API</li>
                 </ul>
               </div>
             </div>
@@ -71,8 +71,12 @@ class Resume extends React.Component {
           </div>
           <div className="section-right">
             <div className="resume-experience">
-              <h2><span className="primary-color">Software Engineer</span> <span className="company-name">DGDean</span></h2>
-              <h3>New York, New York | July 2017 - Present</h3>
+              <div className="resume-header">
+                <h2>Software Engineer</h2>
+                <h3>DGDean</h3>
+              </div>
+              {/* <h2><span className="primary-color">Software Engineer</span> <span className="company-name">DGDean</span></h2> */}
+              <h4>New York, New York | July 2017 - Present</h4>
               <ul>
                 <li>Co-developer for Sweet Defeat platform.</li>
                 <li>Developed an internal data manager for Steady, using Node.js and Angular 5.</li>
@@ -80,14 +84,30 @@ class Resume extends React.Component {
               </ul>
             </div>
             <div className="resume-experience">
-              <h2><span className="primary-color">Consultant Web Developer</span> <span className="company-name">New York City Transit</span></h2>
-              <h3>New York, New York | March 2015 - March 2017</h3>
+              <div className="resume-header">
+                <h2>Consultant Web Developer</h2>
+                <h3>New York City Transit</h3>
+              </div>
+              <h4>New York, New York | March 2015 - March 2017</h4>
               <ul>
                 <li>Built data visualizations for subway and bus performance, using BIRT reports, JavaScript, jQuery, HTML5, and SQL.</li>
                 <li>Created a dashboard for real-time data display, using Node.js, React.js, Socket.IO, and RabbitMQ.</li>
                 <li>Worked on adapting open-source projects for visualizing scheduled and real-time train movement, using Python, JavaScript (D3.js library), JSON, SQL, and Bootstrap.</li>
                 <li>Queried from an Oracle database, which included data for subway and bus ridership, daily arrival and departure times, and station information.</li>
                 <li>Used real-time and scheduled data feeds to build static JSON files.</li>
+              </ul>
+            </div>
+            <div className="resume-experience">
+              <div className="resume-header">
+                <h2>Network Engineer</h2>
+                <h3>Kraft Kennedy</h3>
+              </div>
+              <h4>New York, New York | July 2014 - October 2014</h4>
+              <ul>
+                <li>Experience in configuring and using SCCM and MDT to image workstations.</li>
+                <li>Built and managed Active Directory, including the DHCP and DNS services.</li>
+                <li>Gained a certification to install and configure Windows Server 2012 R2 (MCSA 70-410).</li>
+                <li>Basic experience with VMware, Microsoft SQL Server, Microsoft Exchange, and Citrix XenApp.</li>
               </ul>
             </div>
           </div>
@@ -98,13 +118,17 @@ class Resume extends React.Component {
           </div>
           <div className="section-right">
             <div className="resume-education">
-              <h2><span className="primary-color">Graduate Center at the City University of New York</span></h2>
-              <h3>New York, New York | 2016 - Present</h3>
+              <div className="resume-header">
+                <h2>Graduate Center at the City University of New York</h2>
+              </div>
+              <h4>New York, New York | 2016 - Present</h4>
               <p>Studying for a Master of Arts in Liberal Studies, specializing in data visualization and digital humanities.</p>
             </div>
             <div className="resume-education">
-              <h2><span className="primary-color">Rensselaer Polytechnic Institute</span></h2>
-              <h3>Troy, New York | 2010 - 2014</h3>
+              <div className="resume-header">
+                <h2>Rensselaer Polytechnic Institute</h2>
+              </div>
+              <h4>Troy, New York | 2010 - 2014</h4>
               <p>Graduated as a Bachelor of Science in Information Technology and Web Science, with a concentration in Civil Engineering.</p>
             </div>
           </div>
